bc51ea22eb38211606cc5e3dec556e1d566d4ab3
gitakichi <s.giakichi@gmail.com>
2021-01-21 23:47:24 +0900

    ヘッダを共通化した

---
+++ template.js
@@ -0,0 +1,10 @@
+function header(){
+	var html = `
+		<h1>ギタキチ工具系列</h1>
+		<ul id="nav">
+		<li><a href="index.html">top<a></li>
+		<li><a href="8x8.html">8x8<a></li>
+		</ul>
+	`;
+	document.write(html);
+}

fdb2ab84896cf290b4460cc261ec7228cce6995b
gitakichi <s.giakichi@gmail.com>
2021-01-26 22:12:25 +0900

    中央寄せにした

--- template.js
+++ template.js
@@ -1,10 +1,12 @@
 function header(){
-	var html = `
+	var html_text = `
 		<h1>ギタキチ工具系列</h1>
 		<ul id="nav">
 		<li><a href="index.html">top<a></li>
 		<li><a href="8x8.html">8x8<a></li>
+		<li><a href="comptemp.html">気温グラフ<a></li>
 		</ul>
 	`;
-	document.write(html);
+	var element = document.getElementById("header");
+	element.innerHTML = html_text;
 }
